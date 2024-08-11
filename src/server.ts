83fec1cbd20390ed62ff2a60dbf43933088b014e
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2024-08-10 21:55:35 -0300

    refactor: create .env

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import fastify from "fastify";
 import { knex } from "./database";
+import crypto from "crypto";
 
 const app = fastify();
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*');
-  return tables
+  const transaction = await knex('transactions').select('*').where('amount', 500);
+
+  return transaction;
 })
 
 app.listen({
