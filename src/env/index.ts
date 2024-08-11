73a8e7f40c05584d326017cc437b704811a9a635
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2024-08-10 22:13:57 -0300

    feat: Update dependencies and add zod package

---
+++ src/env/index.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+	NODE_ENV: z
+		.enum(['development', 'test', 'production'])
+		.default('development'),
+	DATABASE_URL: z.string(),
+	PORT: z.number().default(3333),
+});
+
+// export const env = envSchema.parse(process.env); 
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+	console.error('⚠ Invalid environment variables!', _env.error.format());
+
+	throw new Error('Invalid environment variables');
+}
+
+export const env = _env.data;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
-import fastify from "fastify";
-import { knex } from "./database";
-import crypto from "crypto";
+import fastify from 'fastify';
+import { knex } from './database';
+import { env } from './env';
 
 const app = fastify();
 
 app.get('/hello', async () => {
-  const transaction = await knex('transactions').select('*').where('amount', 500);
+	const transaction = await knex('transactions')
+		.select('*')
+		.where('amount', 500);
 
-  return transaction;
-})
+	return transaction;
+});
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('Server is running on port 3333')
-})
+app
+	.listen({
+		port: env.PORT,
+	})
+	.then(() => {
+		console.log('Server is running on port 3333');
+	});
