2f7d4a9667657b0ef35c1ac677fb4607d34ac6e3
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2024-08-13 00:03:18 -0300

    feat: Add transactions route for creating transactions

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
 import fastify from 'fastify';
-import { knex } from './database';
 import { env } from './env';
+import { transactionsRoutes } from './routes/transactions';
 
 const app = fastify();
 
-app.get('/hello', async () => {
-	const transaction = await knex('transactions')
-		.select('*')
-		.where('amount', 500);
-
-	return transaction;
-});
+app.register(transactionsRoutes, { prefix: '/transactions' });
 
 app
 	.listen({
