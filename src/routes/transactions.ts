cb2e116679fcb4b3d08eab2eb6f1e01326fe30ef
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2024-08-16 00:09:04 -0300

    feat: Add checkSessionIdExists middleware
    The code changes add a new middleware function `checkSessionIdExists` in the `check-session-id-exists.ts` file. This middleware checks if the `sessionId` cookie exists in the request and returns a 401 Unauthorized response if it doesn't. This feature enhances the security of the application by ensuring that only authorized users can access certain routes.

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,33 +2,65 @@ import { FastifyInstance } from 'fastify';
 import { knex } from '../database';
 import { z } from 'zod';
 import { randomUUID } from 'crypto';
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-	app.get('/', async () => {
-		const transactions = await knex('transactions').select();
+	app.get(
+		'/',
+		{
+			preHandler: [checkSessionIdExists],
+		},
+		async (request) => {
+			const { sessionId } = request.cookies;
 
-		return { transactions };
-	});
+			const transactions = await knex('transactions')
+				.where('session_id', sessionId)
+				.select();
 
-	app.get('/:id', async (request) => {
-		const getTransactionParamsSchema = z.object({
-			id: z.string().uuid(),
-		});
+			return { transactions };
+		}
+	);
 
-		const { id } = getTransactionParamsSchema.parse(request.params);
+	app.get(
+		'/:id',
+		{
+			preHandler: [checkSessionIdExists],
+		},
+		async (request) => {
+			const getTransactionParamsSchema = z.object({
+				id: z.string().uuid(),
+			});
 
-		const transaction = await knex('transactions').where('id', id).first(); // Sem o first, retorna uma array. O first retorna o primeiro (e único) elemento da array
+			const { id } = getTransactionParamsSchema.parse(request.params);
+			const { sessionId } = request.cookies;
 
-		return { transaction };
-	});
+			const transaction = await knex('transactions')
+				.where({
+					id,
+					session_id: sessionId,
+				})
+				.first(); // Sem o first, retorna uma array. O first retorna o primeiro (e único) elemento da array
 
-	app.get('/summary', async () => {
-		const summary = await knex('transactions')
-			.sum('amount', { as: 'amount' })
-			.first();
+			return { transaction };
+		}
+	);
 
-		return { summary };
-	});
+	app.get(
+		'/summary',
+		{
+			preHandler: [checkSessionIdExists],
+		},
+		async (request) => {
+			const { sessionId } = request.cookies;
+
+			const summary = await knex('transactions')
+				.where('session_id', sessionId)
+				.sum('amount', { as: 'amount' })
+				.first();
+
+			return { summary };
+		}
+	);
 
 	app.post('/', async (request, response) => {
 		const createTransactionBodySchema = z.object({
@@ -41,14 +73,14 @@ export async function transactionsRoutes(app: FastifyInstance) {
 			request.body
 		);
 
-		let sessionId = request.cookies.sessionId
+		let sessionId = request.cookies.sessionId;
 
-		if(!sessionId) {
-			sessionId = randomUUID()
+		if (!sessionId) {
+			sessionId = randomUUID();
 			response.cookie('sessionId', sessionId, {
 				path: '/',
 				maxAge: 60 * 60 * 24 * 7, // 7 days
-			})
+			});
 		}
 
 		await knex('transactions').insert({
